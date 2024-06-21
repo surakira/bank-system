58afae05f6ecf3eda53b53dfbb6d7317d7dc63a6
yeti liyu <surakira2017@gmail.com>
2024-06-21 11:41:38 +0300

    first next

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+
+export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <html lang="en">
+        <main>
+          
+          {children}
+        </main>
+      </html>
+    );
+  }
